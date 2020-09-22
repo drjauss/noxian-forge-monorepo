e31e216fe779f62d7af373806df826f449472032
Jaussman Sanchez <jaussman.sanchez@rappi.com>
2020-09-22 11:18:00 -0500

    feat: removed dependency from api and api interfaces library from user-platform

--- apps/user-platform/src/app/app.module.ts
+++ apps/user-platform/src/app/app.module.ts
@@ -1,13 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [],
+  imports: [BrowserModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
