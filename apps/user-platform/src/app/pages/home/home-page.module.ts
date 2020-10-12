22144ca907605ec9a06143c8b9b071378e1ec000
Jaussman Sanchez <jaussman.sanchez@ktalogapp.com>
2020-10-12 14:18:22 -0500

    feat: initial default template modules for header and footer sections

--- apps/user-platform/src/app/pages/home/home-page.module.ts
+++ apps/user-platform/src/app/pages/home/home-page.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DefaultTemplateComponent } from '../../templates/default/default-template.component';
-import { DefaultTemplateModule } from '../../templates/default/default-template.module';
+import { DefaultTemplateModule } from '../../templates/default/default-template.component.module';
 
 const routes: Routes = [
   {
--- apps/user-platform/src/app/templates/default/default-template.component.module.ts
+++ apps/user-platform/src/app/templates/default/default-template.component.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DefaultTemplateComponent } from './default-template.component';
 
+import { HeaderSectionModule } from '../../sections/header/header-section.component.module';
+import { FooterSectionModule } from '../../sections/footer/footer-section.component.module';
+
 @NgModule({
   declarations: [DefaultTemplateComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderSectionModule, FooterSectionModule],
   exports: [DefaultTemplateComponent],
 })
 export class DefaultTemplateModule {}
